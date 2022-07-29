d20524fc595482fe209946d0bf94ba5d411dc5e7
NKDuy <kn145660@gmail.com>
2022-07-29 17:17:34 +0700

    Dynamic classes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Kdu.createApp({
             url: 'https://nkduy-cloud.web.app',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
-                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg' },
-                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg' },
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
             ]
         }
     },
