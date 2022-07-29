8ac9f55a97679e99570e728095b881c5594554bb
NKDuy <kn145660@gmail.com>
2022-07-29 17:24:33 +0700

    Challenge: toggling favs

--- app.js
+++ app.js
@@ -13,8 +13,15 @@ const app = Kdu.createApp({
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav
         }
     }
 });
 
 app.mount("#app");
+
+// Challenge - Add to Favs
+//   - attach a click event to each li tag (for each book)
+//   - when a user clicks an li, toggle the 'isFav' property of that book
